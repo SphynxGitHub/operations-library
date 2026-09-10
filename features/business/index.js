b07ed3320b5f986af1351ec9f87e50a0c4640863
Sphynx Team <team@sphynxautomation.com>
2026-09-10 14:06:51 -0400

    Create index.js to export business feature modules

---
+++ features/business/index.js
@@ -0,0 +1,13 @@
+import './dashboard.js';
+import './tasks.js';
+import './time-reports.js';
+import './financials.js';
+import './communications.js';
+import './calendar.js';
+
+export const renderDailyDashboard = OL.renderDailyDashboard;
+export const renderBusinessTaskManager = OL.renderBusinessTaskManager;
+export const renderBusinessTimeReports = OL.renderBusinessTimeReports;
+export const renderBusinessFinancials = OL.renderBusinessFinancials;
+export const renderBusinessCommunications = OL.renderBusinessCommunications;
+export const renderBusinessCalendar = OL.renderBusinessCalendar;
